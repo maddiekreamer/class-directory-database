a1dc5625e819c4ca318462e0452b7e1ebf19ba93
Maddie Kreamer <maddiekreamer@gmail.com>
2018-09-14 13:11:33 -0600

    Get route added

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`listening on ${port}`)
 })
+
+app.get("/", (request, response, next) => {
+    response.send('The route worked!')
+});
